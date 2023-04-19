dd91c1f688b1a76d7a9ad8d9b5005deca01dab26
SrStorm20 <131215055+SrStorm20@users.noreply.github.com>
2023-04-19 08:01:01 -0500

    Update ejercicio 1.py

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -1,31 +1,26 @@
 from modulos import  mostrar_mensaje, verificar_entrada
 
 MINIMO = 0
+MULTIPLO = 3
+
 def main ():
     numero1 = verificar_entrada("Ingrese el primer numero: ", MINIMO)
     numero2 = verificar_entrada ("Ingrese el segundo numero (debe ser mayor que el primero): ", numero1)
-    multiplos = imprimir_multiplos (numero1, numero2)
-    mensaje = generar_mensaje (multiplos)
-    mostrar_mensaje (mensaje)
-
-
+    lista = imprimir_multiplos (numero1, numero2)
+    mostrar_mensaje (lista)
 
 def imprimir_multiplos (numero1, numero2):
     multiplos = 0
-    while numero1 <= numero2:
-        if numero1 % 3 == 0 or numero1 == 1:
-            print(f"{numero1} es multiplo de 3")
+    lista = ""
+    i = numero1
+    while i <= numero2:
+        if i % MULTIPLO == 0 or i == 1:
+            lista += f"{i} es multiplo de 3\n"
             multiplos +=1
         else: 
-            print(f"{numero1} no es multiplo de 3")
-        numero1 += 1
-    return multiplos
-
-def generar_mensaje(multiplos):
-    mensaje = f"El total de multiplos de tres en el intervalo es de {multiplos}"
-    return mensaje
-
-
-
+            lista += f"{i} no es multiplo de 3\n"
+        i += 1
+    lista += f"El total de multiplos de tres en el intervalo es de {multiplos}"
+    return lista
 
 main()
